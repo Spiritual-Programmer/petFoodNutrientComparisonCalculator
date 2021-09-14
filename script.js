e5bf3f5c609ed0018ef93167bbcb07d1b133b11d
K.S <kamaljot8165@gmail.com>
2021-09-14 13:37:39 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -55,10 +55,10 @@ function animal() { //Animal type Radio Buttons. Decides to populate either cat 
   selection = document.getElementsByName("animal");
   for (let i of selection) {
     main.style.display = "none";
-    dogFoodBrands.value = 0;
-    catFoodBrands.value = 0;
-    dogRecipes.value = 1;
-    catRecipes.value = 1;
+    dogFoodBrands.value = -1;
+    catFoodBrands.value = -1;
+    dogRecipes.value = 0;
+    catRecipes.value = 0;
     content.innerHTML = "Select a pet food brand to get a nutrient comparison";
     if (i.checked && i.id == "cat") {
         document.querySelector(".dog-food-brand-comparison-container").style.display = "none";
@@ -72,210 +72,129 @@ function animal() { //Animal type Radio Buttons. Decides to populate either cat 
 }
 
 function showDogRecipe() { //Displays current NomNom Recipe upon the selection of the other brands
-  let recipeValue = document.getElementById("dog-recipes").value;
-  logo_img.src = "https://www.nomnomnow.com/images/logo/nom_nom_straight.svg";
-  if (recipeValue == 1) {
-    recipe_name.innerHTML = "BEEF MASH";
-    brand_img2.src="https://www.nomnomnow.com/images/recipes/bowl_beef_square.png";
-    protein_label2.innerHTML = "Protein *";
-    protein_amount2.innerHTML = "93";
-    fat_label2.innerHTML = "Fat";
-    fat_amount2.innerHTML = "48";
-    carb_label2.innerHTML = "Carbohydrate";
-    carb_amount2.innerHTML = "75";
-  }
-  else if (recipeValue == 2) {
-    recipe_name.innerHTML = "CHICKEN CUISINE";
-    brand_img2.src="https://www.nomnomnow.com/images/recipes/bowl_chicken_square.png";
-    protein_label2.innerHTML = "Protein *";
-    protein_amount2.innerHTML = "80";
-    fat_label2.innerHTML = "Fat";
-    fat_amount2.innerHTML = "56";
-    carb_label2.innerHTML = "Carbohydrate";
-    carb_amount2.innerHTML = "71";
-  }
-  else if (recipeValue == 3) {
-    recipe_name.innerHTML = "TURKEY FARE";
-    brand_img2.src="https://www.nomnomnow.com/images/recipes/bowl_turkey_square.png";
-    protein_label2.innerHTML = "Protein *";
-    protein_amount2.innerHTML = "85";
-    fat_label2.innerHTML = "Fat";
-    fat_amount2.innerHTML = "41";
-    carb_label2.innerHTML = "Carbohydrate";
-    carb_amount2.innerHTML = "103";
-  }
-  else if (recipeValue == 4) {
-    recipe_name.innerHTML = "PORK POTLUCK";
-    brand_img2.src="https://www.nomnomnow.com/images/recipes/bowl_pork_square.png";
-    protein_label2.innerHTML = "Protein *";
-    protein_amount2.innerHTML = "76";
-    fat_label2.innerHTML = "Fat";
-    fat_amount2.innerHTML = "48";
-    carb_label2.innerHTML = "Carbohydrate";
-    carb_amount2.innerHTML = "93";
-  }
+  let recipeValue = document.getElementById("dog-recipes");
+  let inputEvent = new Event('input');
+  recipeValue .dispatchEvent(inputEvent)
 }
 
 function showCatRecipe() {////Displays current NomNom Recipe upon the selection of the other brands
-  let recipeValue = document.getElementById("cat-recipes").value;
+  let recipeValue = document.getElementById("cat-recipes");
+  let inputEvent = new Event('input');
+  recipeValue .dispatchEvent(inputEvent)
+}
+
+function changeBrandDetails (cText,bImage,pAmount,fAmount,cAmount) {
+  content.innerHTML = cText;
+  brand_img.src= bImage;
+  protein_label.innerHTML = "Protein";
+  protein_amount.innerHTML = pAmount;
+  fat_label.innerHTML = "Fat";
+  fat_amount.innerHTML = fAmount;
+  carb_label.innerHTML = "Carbohydrate";
+  carb_amount.innerHTML = cAmount;
+}
+
+function changeRecipeDetails (rName,bImage,pAmount,fAmount,cAmount){
   logo_img.src = "https://www.nomnomnow.com/images/logo/nom_nom_straight.svg";
-  if (recipeValue == 1) {
-    recipe_name.innerHTML = "CHICKEN CUISINE";
-    brand_img2.src="https://www.nomnomnow.com/images/recipes/bowl_cat_chicken_square.png";
-    protein_label2.innerHTML = "Protein *";
-    protein_amount2.innerHTML = "163";
-    fat_label2.innerHTML = "Fat";
-    fat_amount2.innerHTML = "42";
-    carb_label2.innerHTML = "Carbohydrate";
-    carb_amount2.innerHTML = "21";
-  }
+  recipe_name.innerHTML = rName;
+  brand_img2.src=bImage;
+  protein_label2.innerHTML = "Protein *";
+  protein_amount2.innerHTML = pAmount;
+  fat_label2.innerHTML = "Fat";
+  fat_amount2.innerHTML = fAmount;
+  carb_label2.innerHTML = "Carbohydrate";
+  carb_amount2.innerHTML = cAmount;
+}
+
+function detailInfo() {
+  protein_info_title.innerHTML="Protein";
+  protein_info.innerHTML="Not all proteins are created equal. Kibble and other more processed foods can use less digestible, lower quality protein.";
+  fat_info_title.innerHTML = "Fat";
+  fat_info.innerHTML = "Fat can oxidize over time, causing issues with digestion. We monitor our fresh diets for oxidation, and the fats are healthier since they are fresh.";
+  carb_info_title.innerHTML = "Carbohydrate";
+  carb_info.innerHTML = "Nom Nom uses only whole food sources of carbohydrates - no grain byproducts, sugars, or starches added.";
+  unit_description.innerHTML = "* All units above are given in grams per 1000 calories (g/kcal)."
+  info_claim.innerHTML = "The information for other brands' foods should be used as a guide only, albeit one that is in most cases better than the percentage. Actual values should be available from the manufacturer. Nom Nom is not liable for any misuse, inaccuracy, or other legal shenanigans related to the above.";
+  btn_h2.innerHTML = "READY TO GO FRESH?";
+  startBtn.innerHTML = "Let's get started";
+  main.style.display = "block";
 }
 
 function showDogFoodBrand(ev) {//Displays the different dog food brands
   let select = ev.target;
-  protein_info_title.innerHTML="Protein";
-  protein_info.innerHTML="Not all proteins are created equal. Kibble and other more processed foods can use less digestible, lower quality protein.";
-  fat_info_title.innerHTML = "Fat";
-  fat_info.innerHTML = "Fat can oxidize over time, causing issues with digestion. We monitor our fresh diets for oxidation, and the fats are healthier since they are fresh.";
-  carb_info_title.innerHTML = "Carbohydrate";
-  carb_info.innerHTML = "Nom Nom uses only whole food sources of carbohydrates - no grain byproducts, sugars, or starches added.";
-  unit_description.innerHTML = "* All units above are given in grams per 1000 calories (g/kcal)."
-  info_claim.innerHTML = "The information for other brands' foods should be used as a guide only, albeit one that is in most cases better than the percentage. Actual values should be available from the manufacturer. Nom Nom is not liable for any misuse, inaccuracy, or other legal shenanigans related to the above.";
-  btn_h2.innerHTML = "READY TO GO FRESH?";
-  startBtn.innerHTML = "Let's get started";
-  main.style.display = "block";
-  if (select.value == 1) {
-    content.innerHTML = "Blue Buffalo ® Wilderness ™ Chicken contains 34% min protein, 15% min fat and has a calorie density of 3599kcal/kg.";
-    brand_img.src="https://www.nomnomnow.com/images/brands/blue_buffalo_chicken.jpg";
-    protein_label.innerHTML = "Protein";
-    protein_amount.innerHTML = "99";
-    fat_label.innerHTML = "Fat";
-    fat_amount.innerHTML = "44";
-    carb_label.innerHTML = "Carbohydrate";
-    carb_amount.innerHTML = "79";  
-  }
-  else if (select.value == 2) {
-    content.innerHTML = "Victor ® Nutra Pro contains 38% min protein, 18% min fat and has a calorie density of 3909kcal/kg.";
-    brand_img.src="https://www.nomnomnow.com/images/brands/victor.jpg";
-    protein_label.innerHTML = "Protein";
-    protein_amount.innerHTML = "101";
-    fat_label.innerHTML = "Fat";
-    fat_amount.innerHTML = "49";
-    carb_label.innerHTML = "Carbohydrate";
-    carb_amount.innerHTML = "67";
-  }
-  else if (select.value == 3) {
-    content.innerHTML = "Purina ® Beneful Beef contains 23% min protein, 12% min fat and has a calorie density of 3558kcal/kg.";
-    brand_img.src="https://www.nomnomnow.com/images/brands/purina_beef.jpg";
-    protein_label.innerHTML = "Protein";
-    protein_amount.innerHTML = "69";
-    fat_label.innerHTML = "Fat";
-    fat_amount.innerHTML = "37";
-    carb_label.innerHTML = "Carbohydrate";
-    carb_amount.innerHTML = "128";
+  detailInfo()
+  dogFoodContent = ["Blue Buffalo ® Wilderness ™ Chicken contains 34% min protein, 15% min fat and has a calorie density of 3599kcal/kg.",
+                    "Victor ® Nutra Pro contains 38% min protein, 18% min fat and has a calorie density of 3909kcal/kg.",
+                    "Purina ® Beneful Beef contains 23% min protein, 12% min fat and has a calorie density of 3558kcal/kg."
+                  ]
+  dogFoodBrandImages = ["https://www.nomnomnow.com/images/brands/blue_buffalo_chicken.jpg",
+                        "https://www.nomnomnow.com/images/brands/victor.jpg",
+                        "https://www.nomnomnow.com/images/brands/purina_beef.jpg"
+                  ]                  
+  dogFoodBrandProteinList = ["99","101","69"]
+  dogFoodBrandFatList = ["44","49","37"]
+  dogFoodBrandCarbList = ["79","67","128"]
+  for (let i = 0; i < select.length-1; i++) {
+    if (select.value == i) {
+    changeBrandDetails(dogFoodContent[i],dogFoodBrandImages[i],dogFoodBrandProteinList[i],
+      dogFoodBrandFatList[i],dogFoodBrandCarbList[i])
+    }
   }
 }
+
 function showCatFoodBrand(ev) {//Displays the different cat food brands
   let select = ev.target;
-  protein_info_title.innerHTML="Protein";
-  protein_info.innerHTML="Not all proteins are created equal. Kibble and other more processed foods can use less digestible, lower quality protein.";
-  fat_info_title.innerHTML = "Fat";
-  fat_info.innerHTML = "Fat can oxidize over time, causing issues with digestion. We monitor our fresh diets for oxidation, and the fats are healthier since they are fresh.";
-  carb_info_title.innerHTML = "Carbohydrate";
-  carb_info.innerHTML = "Nom Nom uses only whole food sources of carbohydrates - no grain byproducts, sugars, or starches added.";
-  unit_description.innerHTML = "* All units above are given in grams per 1000 calories (g/kcal)."
-  info_claim.innerHTML = "The information for other brands' foods should be used as a guide only, albeit one that is in most cases better than the percentage. Actual values should be available from the manufacturer. Nom Nom is not liable for any misuse, inaccuracy, or other legal shenanigans related to the above.";
-  btn_h2.innerHTML = "READY TO GO FRESH?";
-  startBtn.innerHTML = "Let's get started";
-  main.style.display = "block";
-  if (select.value == 1) {
-    content.innerHTML = "Blue Buffalo ® Wilderness ™ Chicken contains 40% min protein, 18% min fat and has a calorie density of 3832kcal/kg.";
-    brand_img.src="https://www.nomnomnow.com/images/brands/blue_buffalo_cat_chicken.jpg";
-    protein_label.innerHTML = "Protein";
-    protein_amount.innerHTML = "108";
-    fat_label.innerHTML = "Fat";
-    fat_amount.innerHTML = "50";
-    carb_label.innerHTML = "Carbohydrate";
-    carb_amount.innerHTML = "57";
-  }
-  else if (select.value == 2) {
-    content.innerHTML = "Iams ® Chicken contains 32% min protein, 15% min fat and has a calorie density of 3740kcal/kg.";
-    brand_img.src="https://www.nomnomnow.com/images/brands/iams_cat_chicken.jpg";
-    protein_label.innerHTML = "Protein";
-    protein_amount.innerHTML = "90";
-    fat_label.innerHTML = "Fat";
-    fat_amount.innerHTML = "43";
-    carb_label.innerHTML = "Carbohydrate";
-    carb_amount.innerHTML = "92";
-  }
-  else if (select.value == 3) {
-    content.innerHTML = "Rachael Ray ® Chicken & Brown Rice contains 34% min protein, 14% min fat and has a calorie density of 3635kcal/kg.";
-    brand_img.src="https://www.nomnomnow.com/images/brands/rachael_ray_cat_chicken.jpg";
-    protein_label.innerHTML = "Protein";
-    protein_amount.innerHTML = "98";
-    fat_label.innerHTML = "Fat";
-    fat_amount.innerHTML = "41";
-    carb_label.innerHTML = "Carbohydrate";
-    carb_amount.innerHTML = "88";
+  detailInfo()
+  catFoodContent = ["Blue Buffalo ® Wilderness ™ Chicken contains 40% min protein, 18% min fat and has a calorie density of 3832kcal/kg.",
+                    "Iams ® Chicken contains 32% min protein, 15% min fat and has a calorie density of 3740kcal/kg.",
+                    "Rachael Ray ® Chicken & Brown Rice contains 34% min protein, 14% min fat and has a calorie density of 3635kcal/kg."
+                  ]
+  catFoodBrandImages = ["https://www.nomnomnow.com/images/brands/blue_buffalo_cat_chicken.jpg",
+                        "https://www.nomnomnow.com/images/brands/iams_cat_chicken.jpg",
+                        "https://www.nomnomnow.com/images/brands/rachael_ray_cat_chicken.jpg"
+                  ]                  
+  catFoodBrandProteinList = ["108","90","98"]
+  catFoodBrandFatList = ["50","43","41"]
+  catFoodBrandCarbList = ["57","92","88"]
+  for (let i = 0; i < select.length-1; i++) {
+    if (select.value == i) {
+    changeBrandDetails(catFoodContent[i],catFoodBrandImages[i],catFoodBrandProteinList[i],
+      catFoodBrandFatList[i],catFoodBrandCarbList[i])
+    }
   }
 }
 
 function showDogNomnomRecipe(ev) {//Displays the dog NomNom recipes
   let select = ev.target;
-  logo_img.src = "https://www.nomnomnow.com/images/logo/nom_nom_straight.svg";
-  if (select.value == 1) {
-    recipe_name.innerHTML = "BEEF MASH";
-    brand_img2.src="https://www.nomnomnow.com/images/recipes/bowl_beef_square.png";
-    protein_label2.innerHTML = "Protein *";
-    protein_amount2.innerHTML = "93";
-    fat_label2.innerHTML = "Fat";
-    fat_amount2.innerHTML = "48";
-    carb_label2.innerHTML = "Carbohydrate";
-    carb_amount2.innerHTML = "75";
-  }
-  else if (select.value == 2) {
-    recipe_name.innerHTML = "CHICKEN CUISINE";
-    brand_img2.src="https://www.nomnomnow.com/images/recipes/bowl_chicken_square.png";
-    protein_label2.innerHTML = "Protein *";
-    protein_amount2.innerHTML = "80";
-    fat_label2.innerHTML = "Fat";
-    fat_amount2.innerHTML = "56";
-    carb_label2.innerHTML = "Carbohydrate";
-    carb_amount2.innerHTML = "71";
-  }
-  else if (select.value == 3) {
-    recipe_name.innerHTML = "TURKEY FARE";
-    brand_img2.src="https://www.nomnomnow.com/images/recipes/bowl_turkey_square.png";
-    protein_label2.innerHTML = "Protein *";
-    protein_amount2.innerHTML = "85";
-    fat_label2.innerHTML = "Fat";
-    fat_amount2.innerHTML = "41";
-    carb_label2.innerHTML = "Carbohydrate";
-    carb_amount2.innerHTML = "103";
-  }
-  else if (select.value == 4) {
-    recipe_name.innerHTML = "PORK POTLUCK";
-    brand_img2.src="https://www.nomnomnow.com/images/recipes/bowl_pork_square.png";
-    protein_label2.innerHTML = "Protein *";
-    protein_amount2.innerHTML = "76";
-    fat_label2.innerHTML = "Fat";
-    fat_amount2.innerHTML = "48";
-    carb_label2.innerHTML = "Carbohydrate";
-    carb_amount2.innerHTML = "93";
+  dogRecipesList = ["BEEF MASH","CHICKEN CUISINE","TURKEY FARE","PORK POTLUCK"]
+  dogRecipeImages = ["https://www.nomnomnow.com/images/recipes/bowl_beef_square.png",
+                     "https://www.nomnomnow.com/images/recipes/bowl_chicken_square.png",
+                     "https://www.nomnomnow.com/images/recipes/bowl_turkey_square.png",
+                     "https://www.nomnomnow.com/images/recipes/bowl_pork_square.png"
+                  ]                  
+  dogRecipeProteinList = ["93","80","85","76"]
+  dogRecipeFatList = ["48","56","41","48"]
+  dogRecipeCarbList = ["75","71","43","93"]
+  for (let i = 0; i < select.length; i++) {
+    if (select.value == i) {
+    changeRecipeDetails(dogRecipesList[i],dogRecipeImages[i],dogRecipeProteinList[i],
+      dogRecipeFatList[i],dogRecipeCarbList[i])
+    }
   }
 }
 
 function showCatNomnomRecipe(ev) {//Displays the cat NomNom recipes
   let select = ev.target;
-  logo_img.src = "https://www.nomnomnow.com/images/logo/nom_nom_straight.svg";
-  if (select.value == 1) {
-    recipe_name.innerHTML = "CHICKEN CUISINE";
-    brand_img2.src="https://www.nomnomnow.com/images/recipes/bowl_cat_chicken_square.png";
-    protein_label2.innerHTML = "Protein *";
-    protein_amount2.innerHTML = "163";
-    fat_label2.innerHTML = "Fat";
-    fat_amount2.innerHTML = "42";
-    carb_label2.innerHTML = "Carbohydrate";
-    carb_amount2.innerHTML = "21";
+  catRecipesList = ["CHICKEN CUISINE"]
+  catRecipeImages = ["https://www.nomnomnow.com/images/recipes/bowl_cat_chicken_square.png",
+                  ]                  
+  catRecipeProteinList = ["163"]
+  catRecipeFatList = ["42"]
+  catRecipeCarbList = ["21"]
+  for (let i = 0; i < select.length; i++) {
+    if (select.value == i) {
+    changeRecipeDetails(catRecipesList[i],catRecipeImages[i],catRecipeProteinList[i],
+      catRecipeFatList[i],catRecipeCarbList[i])
+    }
   }
 }
